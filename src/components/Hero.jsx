d6bb7e55174ce95cb6d89a5ae3e659f1410697af
IsabelGuMo <gm.isabel@gmail.com>
2022-04-26 16:04:01 +0200

    Cambio colores verano

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,22 @@ import './Hero.css';
 
 const Hero = ({ hero }) => {
  return (
-    <div className="hero">
+  <div className="hero">
      <img src={hero.image} alt="Foto Isabel Gutiérrez" />
     <div className="card">
-    <h2>
-      {hero.name} {hero.adress}
-    </h2>
-      <p>{hero.city} </p>
-      <p>
-        <a href={"mailto:" + hero.email}>Mi email</a>
-      </p>
-      <p>{hero.phone}</p>
-      <p>
-        <a href={hero.gitHub}>GitHub</a>
-      </p>
+      <h2>
+        {hero.name} {hero.adress}
+      </h2>
+        <p>{hero.city} </p>
+        <p>
+          <a href={"mailto:" + hero.email}>Mi email</a>
+        </p>
+        <p>
+        <a target="_blank" href={hero.cv} rel="noreferrer">Imprimir CV</a>
+        </p>
+        <p>
+          <a href={hero.gitHub}>GitHub</a>
+        </p>
     </div>
   </div>
  );
